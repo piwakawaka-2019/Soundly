418b6481953459cf72a14bd66391844a4c70dd74
reidvr <reidvr@gmail.com>
2019-04-12 14:24:21 +1200

    combine features

--- components/Soundly.jsx
+++ components/Soundly.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 let images = {
     "a": ["apple", "sound (1)"],
     "b": ["bear", "sound (2)"],
-    "c": ["coconut", "sound (3)"]
+    "c": ["coconut", "sound (3)"],
+    "d": ["dolphin", "sound (4)"],
+    "e": ["jaguar", "sound (5)"],
+    "f": ["kiwifruit", "sound (6)"],
+    "i": ["orange", "sound (7)"],
+    "j": ["peach", "sound (8)"],
+    "k": ["strawberry", "sound (9)"],
+    "l": ["apple", "sound (10)"],
+    "m": ["bear", "sound (11)"],
+    "n": ["coconut", "sound (12)"],
+    "o": ["apple", "sound (13)"],
+    "p": ["bear", "sound (14)"],
+    "q": ["coconut", "sound (15)"],
+    "r": ["apple", "sound (16)"],
+    "s": ["bear", "sound (17)"],
+    "t": ["coconut", "sound (18)"],
+    "u": ["apple", "sound (19)"],
+    "v": ["bear", "sound (20)"],
+    "w": ["coconut", "sound (21)"],
+    "x": ["apple", "sound (22)"],
+    "y": ["bear", "sound (23)"],
+    "z": ["coconut", "sound (24)"],
 }
 
 class Soundly extends React.Component {
@@ -14,11 +35,6 @@ class Soundly extends React.Component {
             keyPushed: '',
             image: '',
             audio: '',
-            style: {
-                height: '250px',
-                width: '300px',
-                backgroundColor: 'grey'
-            }
         }
     }
 
@@ -38,8 +54,8 @@ class Soundly extends React.Component {
 
     render(){
         return (
-            <div style={this.state.style} id="soundly">
-                <img src ={this.state.image}/>
+            <div id="soundly">
+                <img src ={this.state.image} id="key-image" />
                 <audio src={this.state.audio} type="audio/wav" autoPlay random={this.state.random} />
             </div>)
     }
